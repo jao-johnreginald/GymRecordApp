93354712eaa06c658708cf521407b3bd815049f1
John Reg <johnreginaldjao@gmail.com>
2024-01-30 22:07:33 +0800

    added ViewBinding in RecyclerViewAdapter

--- app/src/main/java/com/johnreg/gymrecord/recyclerview/RecyclerViewAdapter.kt
+++ app/src/main/java/com/johnreg/gymrecord/recyclerview/RecyclerViewAdapter.kt
@@ -3,11 +3,9 @@ package com.johnreg.gymrecord.recyclerview
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.johnreg.gymrecord.R
+import com.johnreg.gymrecord.databinding.ItemWorkoutBinding
 
 class RecyclerViewAdapter(
     private val workouts: Array<Workout>
@@ -15,30 +13,31 @@ class RecyclerViewAdapter(
 
     private var onClickListener: OnClickListener? = null
 
-    override fun getItemCount(): Int {
-        return workouts.size
-    }
+    override fun getItemCount(): Int = workouts.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_workout, parent, false)
+        val inflater = LayoutInflater.from(parent.context)
+        val itemView = inflater.inflate(R.layout.item_workout, parent, false)
         return ViewHolder(itemView, onClickListener)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentItem = workouts[position]
-        holder.image.setImageResource(currentItem.image)
-        holder.textTitle.text = currentItem.textTitle
-        holder.textRecord.text = currentItem.textRecord
-        holder.textDate.text = currentItem.textDate
-        holder.rating.rating = currentItem.rating
+        holder.bind(currentItem)
     }
 
-    class ViewHolder(itemView: View, onClickListener: OnClickListener?) : RecyclerView.ViewHolder(itemView) {
-        val image: ImageView = itemView.findViewById(R.id.imageViewItem)
-        val textTitle: TextView = itemView.findViewById(R.id.textViewImageTitle)
-        val textRecord: TextView = itemView.findViewById(R.id.textViewRecordValue)
-        val textDate: TextView = itemView.findViewById(R.id.textViewDateValue)
-        val rating: RatingBar = itemView.findViewById(R.id.ratingBar)
+    class ViewHolder(
+        itemView: View,
+        onClickListener: OnClickListener?
+    ) : RecyclerView.ViewHolder(itemView) {
+        fun bind(currentItem: Workout) {
+            val binding = ItemWorkoutBinding.bind(itemView)
+            binding.imageViewItem.setImageResource(currentItem.image)
+            binding.textViewImageTitle.text = currentItem.textTitle
+            binding.textViewRecordValue.text = currentItem.textRecord
+            binding.textViewDateValue.text = currentItem.textDate
+            binding.ratingBar.rating = currentItem.rating
+        }
 
         init {
             itemView.setOnClickListener {
@@ -51,8 +50,8 @@ class RecyclerViewAdapter(
         fun onClick(position: Int)
     }
 
-    fun setOnClickListener(parameter: OnClickListener) {
-        onClickListener = parameter
+    fun setOnClickListener(listener: OnClickListener) {
+        onClickListener = listener
     }
 
 }
